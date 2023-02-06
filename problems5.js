6ab3e26d4169610b07abc23b3959a969c4c2b1c5
biswasbittu <biswasbittu7415@yahoo.com>
2023-02-06 17:02:35 +0530

    problems 6

--- problems5.js
+++ problems5.js
@@ -1,2 +1,18 @@
+/*
+তোমাকে যদি বলা হয়। একটা array এর মধ্যে ৮০ এর চাইতে বড় সংখ্যা থাকলে সেগুলাকে console log করে দেখাতে সেটা কি তুমি পারবে? 
+তাহলে তুমি সেই কোড করে ফেলো 
+*/
 
+let num=[23,90,50,444,267,18,60,50,35,60,430,650,280]
+// console.log(num.length)
+
+for(i=0;i<num.length;i++){
+    let element=num[i];
+    // console.log(element);
+    
+    if(element>80){
+        // console.log('The number that gratterthan 80 is:',element);
+        console.log(element);
+    }
+}
 
---
+++ problems6.js
@@ -0,0 +1,6 @@
+function product(num1,num2,num3){
+    const finalResult=num1*num2*num3;
+    return finalResult;
+}
+let numbers=product(1,5,3);
+console.log(numbers);
